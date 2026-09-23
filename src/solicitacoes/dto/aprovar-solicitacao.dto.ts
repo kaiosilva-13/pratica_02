dc8655ae567db65b51d54d8e2b719f0a611703fe
kaiosilva-13 <kaiovitor13052005@gmail.com>
2026-09-23 17:01:42 -0300

    alteração nos campos de dto de solicitacao

--- src/solicitacoes/dto/aprovar-solicitacao.dto.ts
+++ src/solicitacoes/dto/aprovar-solicitacao.dto.ts
@@ -1,7 +1,12 @@
-import { IsInt, Min } from "class-validator";
+import { IsInt, Min, IsString, MinLength, MaxLength} from "class-validator";
 
 export class AprovarSolicitacaoDto {
   @IsInt()
   @Min(1)
   versao: number;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
+  centroCusto: string;
 }
